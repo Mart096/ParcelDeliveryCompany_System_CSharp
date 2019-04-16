f567c05420f61fed8697d0b72a7e49b9c4d80019
CrazyProgrammer <marcin.surma@protonmail.com>
2019-04-16 11:49:16 +0200

    Zmodyfikowano właściwość Anchor elementu w CustomerAddEditForm.

--- TestSQLServerProject01/AddEditForms/CustomerAddEditForm.Designer.cs
+++ TestSQLServerProject01/AddEditForms/CustomerAddEditForm.Designer.cs
@@ -57,6 +57,9 @@
             // 
             // customerAddEdit_Controler
             // 
+            this.customerAddEdit_Controler.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.customerAddEdit_Controler.Location = new System.Drawing.Point(12, 12);
             this.customerAddEdit_Controler.MainWindowReference = null;
             this.customerAddEdit_Controler.Name = "customerAddEdit_Controler";
