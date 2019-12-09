a979794dc3047d7d3b86791eefa0b49b865a9c42
Marcin <marcin.surma@protonmail.com>
2019-12-09 09:51:11 +0000

    Delete OrdersListQuery.sql

--- OrdersListQuery.sql
+++
@@ -1,7 +0,0 @@
-﻿SELECT z.Id_zlecenia AS 'Numer zlecenia', 
-k.Id_klienta AS 'Id klienta', k.Imie AS 'Imię', k.Nazwisko, k.Ulica, k.Nr_domu AS 'Nr domu', 
-k.Nr_mieszkania AS 'Nr mieszkania', k.Miasto, k.Kod_pocztowy AS 'Kod pocztowy',
-z.Id_kuriera AS 'Kurier obsługujący zlecenie' 
-FROM Zlecenie z 
-INNER JOIN Klient k ON z.Id_klienta=k.Id_klienta 
-WHERE Id_zlecenia = 1;
