d9a00129a081ac6a2cfae9628ed265693c08848f
Marcin <marcin.surma@protonmail.com>
2019-12-09 09:50:58 +0000

    Delete zapytanie_cechy_paczki.sql

--- zapytanie_cechy_paczki.sql
+++
@@ -1,9 +0,0 @@
-declare @result varchar(max)
-
-select @result = COALESCE(@result + ', ', '') + Cecha
-from Cecha
-WHERE Id_cechy IN (SELECT Id_cechy_paczki FROM Cecha_paczki WHERE Id_paczki = 1)
-
-select @result
-
-SELECT * FROM Paczka
